8fb9df8f38a72cb2f6cb4dc2f6f290c12452ef13
sagar.bh <sagarbhojaviya.spaceo@gmail.com>
2023-01-24 16:59:15 +0530

    - removed code

--- app/src/main/java/com/example/githublibrary/MainActivity.kt
+++ app/src/main/java/com/example/githublibrary/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.githublibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sagarlibrary.TestClick
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       // TestClick.blink()
     }
 }
